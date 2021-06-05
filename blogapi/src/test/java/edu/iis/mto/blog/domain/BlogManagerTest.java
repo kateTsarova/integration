e01548a9d0d54ece5a61cccb2fd11f5054fe9687
kateryna <katerynatsarova@gmail.com>
2021-06-05 17:34:51 +0300

    added tests that throwing domain error when user is unconfirmed

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,8 +1,26 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.services.BlogService;
+import org.junit.jupiter.api.BeforeEach;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.verify;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,12 +43,32 @@ class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+    @MockBean
+    private LikePostRepository likePostRepository;
 
     @Autowired
     private BlogService blogService;
 
     @Captor
     private ArgumentCaptor<User> userParam;
+
+    private User dummyUser;
+    private BlogPost dummyBlogPost;
+
+    @BeforeEach
+    void setUp() {
+        dummyUser = new User();
+        dummyUser.setAccountStatus(AccountStatus.CONFIRMED);
+        dummyUser.setEmail("kowalski@mail.com");
+        dummyUser.setId(1L);
+
+        dummyBlogPost = new BlogPost();
+        dummyBlogPost.setEntry("Dummy Entry");
+        dummyBlogPost.setUser(dummyUser);
+        dummyBlogPost.setId(1L);
+    }
 
     @Test
     void creatingNewUserShouldSetAccountStatusToNEW() {
@@ -40,4 +78,11 @@ class BlogManagerTest {
         assertThat(user.getAccountStatus(), equalTo(AccountStatus.NEW));
     }
 
+    @Test
+    void shouldThrowDomainErrorWhenUnconfirmedUserLikesPost() {
+        when(userRepository.findById(any())).thenReturn(Optional.of(dummyUser));
+        when(blogPostRepository.findById(any())).thenReturn(Optional.of(dummyBlogPost));
+
+        assertThrows(DomainError.class, () -> blogService.addLikeToPost(dummyUser.getId(), dummyBlogPost.getId()));
+    }
 }
