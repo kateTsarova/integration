e3396c52b2e38dab41b88c78c2453deaa3db164a
kateryna <katerynatsarova@gmail.com>
2021-06-05 18:59:03 +0300

    added tests in GetUserTest (find user by email/name)

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserTest.java
@@ -0,0 +1,46 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetUserTest extends FunctionalTests {
+
+    private static final String GET_USER_API = "/blog/user/find";
+
+    @Test
+    void shouldReturnUserByEmailWhenPassedEmail() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "john@domain.com")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(GET_USER_API);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(1));
+    }
+
+    @Test
+    void shouldReturnUserByNameWhenPassedName() {
+        Response response = given().accept(ContentType.JSON)
+                .queryParam("searchString", "John")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(GET_USER_API);
+
+        JSONArray responseBody = new JSONArray(response.getBody().asString());
+        assertThat(responseBody.length(), equalTo(1));
+    }
+}
